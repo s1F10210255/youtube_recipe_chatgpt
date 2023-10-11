4274c0d62f544f9e7396ee066353f3f1033d917d
s1F10210255 <s1f102102559@iniad.org>
2023-10-11 13:39:01 +0900

    push2
    localhostで問題

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:youtube_api/youtube_api.dart';
+import 'package:firebase_core/firebase_core.dart';  // Import the Firebase Core package
+import 'firebase_options.dart';  // Import the generated Firebase options file
+import 'package:youtube_recipe/summary_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Ensure flutter binding
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  // Initialize Firebase with the default options
+  runApp(MyApp());  // Run the app
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Redirect Test'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: _checkRedirect,
-            child: Text('Check Redirect'),
-          ),
-        ),
+      home: DemoApp(),
+    );
+  }
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  _DemoAppState createState() => _DemoAppState();
+}
+
+class _DemoAppState extends State<DemoApp> {
+  static String key = "AIzaSyA8OXpQMoeDgbb7nkwX4mDpjeCh4UmQkOQ";
+
+  YoutubeAPI youtube = YoutubeAPI(key);
+  List<YouTubeVideo> videoResult = [];
+
+  Future<void> callAPI() async {
+    String query = "レシピ";
+    videoResult = await youtube.search(
+      query,
+      order: 'viewCount',
+      videoDuration: 'any',
+      regionCode: 'JP',
+    );
+    videoResult = await youtube.nextPage();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    callAPI();
+    print('hello');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text('Youtube API'),
+      ),
+      body: ListView(
+        children: videoResult.map<Widget>(listItem).toList(),
       ),
     );
   }
 
-  void _checkRedirect() async {
-    final response = await http.get(Uri.parse('http://localhost:5000'));
-    if (response.statusCode == 302) {  // 302 is the HTTP status code for redirection
-      print('Redirected to: ${response.headers['location']}');
-    } else {
-      print('No redirect');
-    }
+  Widget listItem(YouTubeVideo video) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SummaryPage(videoId: video.id ?? ''),
+          ),
+        );
+      },
+      child: Card(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 7.0),
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Image.network(
+                  video.thumbnail.small.url ?? '',
+                  width: 120.0,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      video.title,
+                      softWrap: true,
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 3.0),
+                      child: Text(
+                        video.channelTitle,
+                        softWrap: true,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      video.url,
+                      softWrap: true,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
